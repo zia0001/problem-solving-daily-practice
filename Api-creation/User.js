a8aad35cf032a52d2b253d8f99565b07a2d7df01
zia0001 <ziawc0001@gmail.com>
2024-11-18 00:08:17 +0500

    Signup_API_Schema created successfully

---
+++ Api-creation/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      min: 2,
+      max: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+      max: 50,
+    },
+    phone:{
+      type: String,
+      unique: true,
+    },
+    Age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+    },
+    profileImage: {
+      type: String,
+      default:"",
+    }
+
+  }
+)
