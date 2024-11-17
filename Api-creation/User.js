2f2fd5e8eba0113fc86278cad4285b46ae6a24eb
zia0001 <ziawc0001@gmail.com>
2024-11-18 00:38:07 +0500

    schema model created successfully

--- Api-creation/User.js
+++ Api-creation/User.js
@@ -33,7 +33,10 @@ const userSchema = new mongoose.Schema(
     profileImage: {
       type: String,
       default:"",
-    }
+    },
+    timestamps: true
 
-  }
-)
+  });
+
+  const User = mongoose.model("User", userSchema);
+  export default User;
