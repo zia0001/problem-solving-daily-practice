3b5d5ec1aaafcb0d7344d0809c88641de50c4067
zia0001 <ziawc0001@gmail.com>
2024-11-18 01:05:06 +0500

    the sum of nums multiples of three or five

---
+++ problem5.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")(); // Importing library the library
+
+const n = parseInt(prompt("Enter a number: "));
+if (!isNaN(n) && n > 0) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      if(i % 3 === 0 || i % 5 === 0){
+        sum += i;
+      }
+    }
+    console.log(`The sum of numbers from 1 to ${n} is ${sum}`);
+} else {
+    console.log("Please enter a valid positive number.");
